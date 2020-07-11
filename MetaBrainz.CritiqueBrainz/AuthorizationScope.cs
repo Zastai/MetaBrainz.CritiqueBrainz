9ef9e9778787cc6065780976571ede49f108ca0e
Tim Van Holder <tim.vanholder@gmail.com>
2020-07-11 18:15:43 +0200

    Enable Dependabot

---
+++ MetaBrainz.CritiqueBrainz/AuthorizationScope.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using JetBrains.Annotations;
+
+namespace MetaBrainz.CritiqueBrainz {
+
+  /// <summary>Enumeration of the scopes available through CritiqueBrainz OAuth2 authentication.</summary>
+  [Flags]
+  [PublicAPI]
+  public enum AuthorizationScope {
+
+    /// <summary>No authorization requested.</summary>
+    None          = 0,
+
+    /// <summary>Request all available permissions (not recommended).</summary>
+    Everything    = -1,
+
+    /// <summary>Create and modify reviews.</summary>
+    Review        = 0x00000001,
+
+    /// <summary>Submit and delete votes on reviews.</summary>
+    Vote          = 0x00000002,
+
+    /// <summary>Modify profile info and delete profile.</summary>
+    User          = 0x00000004,
+
+  }
+
+}
